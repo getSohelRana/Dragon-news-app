340c1123187ef9db4315581ccbc78e6ed778b53f
getSohelRana <getsohelinfo@gmail.com>
2025-10-26 08:11:40 +0600

    local time feature added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="flex flex-col justify-center items-center mt-13">
         <img src={siteLogo} alt="site-logo" />
         <h1 className="text-[18px] mt-3 text-accent">Journalism Without Fear or Favor</h1>
-        <p className="mt-4 text-accent font-semibold">{format(new Date() , 'EEEE, MMMM dd, yyyy')}</p>
+        <p className="mt-4 text-accent font-semibold">{format(new Date() , 'p, EEEE, MMMM dd, yyyy')}</p>
       </div>
     </div>
   );
